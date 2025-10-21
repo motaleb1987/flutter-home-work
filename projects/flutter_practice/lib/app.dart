f658bfd060325bac61e937a88f59a004e658c3c7
Motaleb Hossain <motaleb.it@gmail.com>
2025-10-21 23:05:32 +0600

    Theme Apply

--- projects/flutter_practice/lib/app.dart
+++ projects/flutter_practice/lib/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_practice/home.dart';
 import 'package:flutter_practice/module_7/class_2_button.dart';
 import 'package:flutter_practice/module_8/class_1_input.dart';
+import 'package:flutter_practice/module_9/theme_test.dart';
 
 
 class MyApp extends StatelessWidget{
@@ -11,11 +12,58 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.grey.shade300,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.orange,
+              foregroundColor: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)
+              )
+            )
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Colors.grey)
+            ),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.orange)
+            ),
+            hintStyle: TextStyle(
+              color: Colors.grey
+            ),
+            labelStyle: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          textTheme: TextTheme(
+            headlineLarge: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            ),
+
+            titleLarge: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              color: Colors.white
+            ),
+
+          )
+        ),
         title: "Fullter Practise",
-      color: Colors.white,
+      //color: Colors.white,
       //home: Home(),
      // home: ButtonGrp(),
-      home: FlutterInput(),
+      //home: FlutterInput(),
+      home: ThemeTest(),
 
     );
 
--- projects/flutter_practice/lib/module_8/class_1_input.dart
+++ projects/flutter_practice/lib/module_8/class_1_input.dart
@@ -10,93 +10,59 @@ class FlutterInput extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login',
+        title: Text('WEBS',
           style: TextStyle(
             fontSize: 25,
             color: Colors.white,
-            fontWeight: FontWeight.bold
+            fontWeight: FontWeight.bold,
           ),
         ),
         backgroundColor: Colors.blue,
+        centerTitle: true,
       ),
 
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            // Image.network('https://clinmedjournals.org/articles/blogs/img/facebook-login.jpg'),
-            Image.asset('assets/img_1.png',
-            height: 250,
-              width: 250,
-            ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              // Image.network('https://clinmedjournals.org/articles/blogs/img/facebook-login.jpg'),
+              Image.asset('assets/img_1.png',
+              height: 200,
+                width: 200,
+              ),
 
-            SizedBox(height: 20,),
-            TextField(
-              controller: phoneController,
-              keyboardType: TextInputType.phone,
-              maxLength: 11,
-              cursorColor: Colors.orange,
-              decoration: InputDecoration(
-                  hintText: 'Enter Your Phone',
-                hintStyle: TextStyle(
-                  fontSize: 18,
-                  color: Colors.grey,
-                ),
-                helperText: '0191xxx',
-                helperStyle: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 16
-                ),
-                labelText: 'Phone Number',
-                labelStyle: TextStyle(
-                  fontSize: 20,
-                  color: Colors.orange,
-                  fontWeight: FontWeight.bold
-                ),
-                prefixIcon: Icon(Icons.phone),
-                suffixIcon: Icon(Icons.check),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: Colors.grey)
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.orange)
-                ),
-                filled: true,
-                fillColor: Colors.grey.shade100,
-                contentPadding: EdgeInsets.all(20),
-              )
-            ),
-            SizedBox(height: 20,),
-            TextField(
-              controller: passwordController,
-                obscureText: true,
+              TextField(
+                controller: phoneController,
+                keyboardType: TextInputType.phone,
+                maxLength: 5,
+                cursorColor: Colors.orange,
                 decoration: InputDecoration(
-                  hintText: 'Password',
+                    hintText: 'Enter Your ID',
                   hintStyle: TextStyle(
                     fontSize: 18,
                     color: Colors.grey,
                   ),
-
-                  labelText: 'Password',
-                  labelStyle: TextStyle(
-                      fontSize: 20,
-                      color: Colors.orange,
-                      fontWeight: FontWeight.bold
+                  helperText: 'Ex: 14930',
+                  helperStyle: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 16
                   ),
-                  prefixIcon: Icon(Icons.lock),
-                  suffixIcon: Icon(Icons.remove_red_eye),
+                  labelText: 'Your ID',
+                  labelStyle: TextStyle(
+                    fontSize: 20,
+                    color: Colors.orange,
+                    fontWeight: FontWeight.bold
+                  ),
+                  prefixIcon: Icon(Icons.person),
+                  suffixIcon: Icon(Icons.check),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
                   enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.grey)
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.grey)
                   ),
                   focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
@@ -106,31 +72,67 @@ class FlutterInput extends StatelessWidget {
                   fillColor: Colors.grey.shade100,
                   contentPadding: EdgeInsets.all(20),
                 )
-            ),
-            SizedBox(height: 20,),
-            SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)
-                      )
+              ),
+              SizedBox(height: 20,),
+              TextField(
+                controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey,
                     ),
-                    onPressed: (){
-                      print(phoneController.text);
-                      print(passwordController.text);
-                      phoneController.clear();
-                      passwordController.clear();
-                    }, child: Text('Login',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
-                ),
-                )))
-          ],
+          
+                    labelText: 'Password',
+                    labelStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.orange,
+                        fontWeight: FontWeight.bold
+                    ),
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: Icon(Icons.remove_red_eye),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.grey)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.orange)
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey.shade100,
+                    contentPadding: EdgeInsets.all(20),
+                  )
+              ),
+              SizedBox(height: 20,),
+              SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      ),
+                      onPressed: (){
+                        print(phoneController.text);
+                        print(passwordController.text);
+                        phoneController.clear();
+                        passwordController.clear();
+                      }, child: Text('Login',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                  ),
+                  )))
+            ],
+          ),
         ),
       ),
     );
